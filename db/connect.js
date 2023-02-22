c9486c3795c6aec2752923a980e8501761c98286
xyfer17 <naveen@feldspartech.com>
2023-02-23 00:31:18 +0530

    fix  db leak

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://xyfer17:naveen12@atlascluster.8uxfwos.mongodb.net/task-manager?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
   mongoose.set("strictQuery", false);
   return mongoose.connect(url);
